fb7d9b0abec37d5ce3d4f9ac894edce3bcf826ed
Vladislav Konovalov <idnno@mail.ru>
2023-04-16 17:13:11 +0300

    Probably this

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -5,7 +5,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls'))
+    path('', include('posts.urls')),
+    path('', include('api.urls')),
 ]
 
 
